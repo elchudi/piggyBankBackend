78bbfe3be3896794ba9dde0d1133c21c1b641d70
elchudi <gustavogiudici@gmail.com>
2012-10-26 22:09:03 +0200

    populating some for testing

--- backed/orm_populate.py
+++ backed/orm_populate.py
@@ -9,11 +9,12 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///piggy.db', echo=False)
 session = sessionmaker(bind=engine)()
 
-acc1 = orm.Account(1, 5000, 64, 1000)
-acc2 = orm.Account(2, 7000, 128, 10000)
+acc1 = orm.Account(5000, 64, 1000)
+acc2 = orm.Account(7000, 128, 10000)
 
-shared1 = orm.SharedAccount(1,1,666666666)
-shared2 = orm.SharedAccount(2,1,666666555)
+shared1 = orm.SharedAccount(1,666666666)
+shared2 = orm.SharedAccount(2,666666666)
+shared3 = orm.SharedAccount(1,666666555)
 
-session.add_all([acc1, acc2, shared1, shared2])
+session.add_all([acc1, acc2, shared1, shared2, shared3])
 session.commit()
